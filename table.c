f2bc84f2005998eef754b95125069cd5ce3a0260
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-12-29 05:34:34 +0000

    New

--- table.c
+++ table.c
@@ -2,7 +2,7 @@
 
 void main(){
   int num,a,i;
-  printf("Enter a number");
+  printf("Enter a number : ");
   scanf("%d",&num);
   for(i = 1;i <= 10;i++){
     printf(" %d X %d = %d\n",num,i,num*i);
