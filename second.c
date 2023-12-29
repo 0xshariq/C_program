8127198d07c219b372281c096f2433f034d28a5d
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-12-29 05:36:49 +0000

    New

--- second.c
+++ second.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int i,f, p;
-     f = scanf("%d",&f);
-     
+    int i, f, p;
+    p = scanf("%d", &f);
+    
     for (i = 1; i <= 10; i++) {
         printf("%d\n", i);
     }
+    
     return 0;
 }
